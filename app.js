a54019034719178cb20aeb75da3ad4769c8fe526
jfourdachon <jfourdachon@yahoo.com>
2020-09-16 23:29:37 +0200

    add express rate-limit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const userRouter = require('./routes/user.route');
 const tourRouter = require('./routes/tour.route');
 const AppError = require('./utils/appError');
@@ -8,13 +9,23 @@ const globalErrorHandler = require('./controllers/error.controller');
 
 const app = express();
 
-// 1 - MIDDLEWARES
+// 1 - GLOBAL MIDDLEWARES
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests per IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+// Can apply this limiter on specific route -> ()app.user('/users', limiter)
+app.use(limiter);
 
 app.use(express.json());
 
